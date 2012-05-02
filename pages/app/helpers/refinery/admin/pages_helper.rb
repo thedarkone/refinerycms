868d93ddbfebc46e47cc87e4beae8d362f94b8dd
thedarkone <thedarkone2@gmail.com>
2012-05-02 16:37:33 +0200

    Manually preload the :translations to avoid the N+1 queries.

--- pages/app/helpers/refinery/admin/pages_helper.rb
+++ pages/app/helpers/refinery/admin/pages_helper.rb
@@ -1,6 +1,14 @@
 module Refinery
   module Admin
     module PagesHelper
+      def parent_id_nested_set_options(page)
+        pages = []
+        nested_set_options(::Refinery::Page, page) {|i| pages << i}
+        # page.title needs the :translations association, doing something like
+        # nested_set_options(::Refinery::Page.includes(:translations), page) doesn't work
+        ActiveRecord::Associations::Preloader.new(pages, :translations).run
+        pages.map {|i, id| ["#{'-' * i.level} #{i.title}", id]}
+      end
     end
   end
 end
